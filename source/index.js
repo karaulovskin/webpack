6a0330c0466779dae88e44eaa633d400ae29e487
karaulovskin <karaulovskin@yandex.ru>
2019-05-14 13:37:29 +0500

    server add

--- source/index.js
+++ source/index.js
@@ -1,3 +1,3 @@
 import createMenu from './menu';
-var menu = createMenu(['Главная', 'Обо мне', 'Портфолио'], 'menu');
+var menu = createMenu(['Главная', 'Обо мне', 'Портфолио', 'Контакты'], 'menu');
 document.body.appendChild(menu);
